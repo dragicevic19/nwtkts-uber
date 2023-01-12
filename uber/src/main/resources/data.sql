764c593a0ce28011722ba5ca51088391d8293d95
Ненад Раковић <nenad.rakovic244@gmail.com>
2023-01-12 23:56:58 +0100

    changes in data.sql for test@gmail.com user to be fully supported

--- uber/src/main/resources/data.sql
+++ uber/src/main/resources/data.sql
@@ -5,9 +5,9 @@
 INSERT INTO address (city, country, street)
 VALUES ('Novi Sad', 'Serbia', 'Dunavska 2');
 -- lozinka je 123
-INSERT INTO users (blocked, email, enabled, first_name, last_name, last_password_reset_date, password, phone_number,
+INSERT INTO users (blocked, email, enabled, first_name, full_reg_done, last_name, last_password_reset_date, password, phone_number,
                    photo, address_id)
-VALUES (false, 'test@gmail.com', true, 'Pera', 'Peric', '2021-10-11 18:57:58.508-07',
+VALUES (false, 'test@gmail.com', true, 'Pera', true, 'Peric', '2021-10-11 18:57:58.508-07',
         '$2a$04$Vbug2lwwJGrvUXTj6z7ff.97IzVBkrJ1XfApfGNl.Z695zqcnPYra', '0665011426', 'slika.jpg', 1);
 insert into admin
 values (1);
